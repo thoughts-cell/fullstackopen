a6826dabce92875527db46185b5dafc704e9d8d8
Zhengxu SUN <zhengxu.sun@cqumail.com>
2025-08-07 21:28:03 +1000

    excercise pt1 table

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,37 @@
 import { useState } from 'react'
+
+
+
+const Statistics = ({ good, neutral, bad }) => {
+  let total = good + neutral + bad  
+  if (total === 0) {
+    return <p>No feedback given</p>
+  }
+  let average = (good - bad) / total
+  let positive = (good / total) * 100
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + ' %'} />
+          </tbody>
+          </table>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  ) 
+}
+
 const App = () => {
   const  [good, setGood] = useState(0)
   const  [neutral, setNeutral] = useState(0)
